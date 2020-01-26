351ff3bfcce560bba99f7629ac48b081e2d211db
Donnie D'Amato <fauxserious@gmail.com>
2020-01-26 11:58:43 -0500

    Prepare api router

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,15 @@
-export async function handler(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: `Hello world ${Math.floor(Math.random() * 10)}` })
-  };
+import { Router } from 'lambda-router';
+const router = Router({
+  logger: console // uses logger-wrapper.
+})
+
+router.get('/', () => {
+  return { message: 'success' }
+});
+
+export async function handler (lambdaEvent, context) {
+  context.callbackWaitsForEmptyEventLoop = false
+  let result = await router.route(lambdaEvent, context)
+ 
+  return result.response;
 }
