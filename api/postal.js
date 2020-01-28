86e47a51550ae1c498656e37b5f84dd3f7597682
Donnie D'Amato <fauxserious@gmail.com>
2020-01-28 09:30:19 -0500

    Fix axios response

--- api/postal.js
+++ api/postal.js
@@ -3,6 +3,10 @@ import Coords from './coords.js';
 
 export default async function getCoordsByPostal(postal) {
   const base = `https://geocode.xyz/?auth=${process.env.GEOCODEXYZ_API_KEY}`;
-  const { latt, longt } = await axios.get(`${base}&locate=${postal}&json=1`);
-  return new Coords(latt, longt);
+  try {
+    const { data } = await axios.get(`${base}&locate=${postal}&json=1`);
+    return new Coords(data.latt, data.longt);
+  } catch (err) {
+    return null;
+  }
 }
