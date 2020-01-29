f64fce7e76c0578359d75b06191df4a0f5b8d8b0
Donnie D'Amato <fauxserious@gmail.com>
2020-01-28 19:09:22 -0500

    Break redirect to test

--- api/rss.js
+++ api/rss.js
@@ -112,7 +112,7 @@ async function writeXML(coords, rssJs) {
   const contents = convert.js2xml(rssJs, { compact: true });
   await s3.putObject({
     Bucket: 'www.deltazeus.com',
-    Key: `${coords}.xml`,
+    Key: `rss/${coords}.xml`,
     ContentType: 'application/xml',
     Body: Buffer.from(`${contents}`, 'utf8')
   }).promise();
