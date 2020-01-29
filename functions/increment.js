4316ad32cb13f3d7c2b3f9004bac92f51fbc06af
Donnie D'Amato <fauxserious@gmail.com>
2020-01-29 13:35:58 -0500

    Add logic back into function

--- functions/increment.js
+++ functions/increment.js
@@ -1,6 +1,14 @@
 import { getRecords, incrementRequests } from '../api/database.js';
 
 export async function handler({ queryStringParameters }) {
+  const { coords } = queryStringParameters || {};
+  if (coords) {
+    increment(coords);
+    return {
+      statusCode: 301,
+      body: `https://rss.deltazeus.com/${coords}.xml`,
+    }
+  }
   return {
     statusCode: 200,
     body: JSON.stringify(queryStringParameters),
