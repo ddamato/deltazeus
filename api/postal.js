1c46b1a2b0640ce4a3b1ba693856cd10e2a3a940
Donnie D'Amato <fauxserious@gmail.com>
2020-02-22 22:57:47 -0500

    Namespace secret keys

--- api/postal.js
+++ api/postal.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Coords from './coords.js';
 
 export default async function getCoordsByPostal(postal) {
-  const base = `https://geocode.xyz/?auth=${process.env.GEOCODEXYZ_API_KEY}`;
+  const base = `https://geocode.xyz/?auth=${process.env.DZ_GEOCODEXYZ_API_KEY}`;
   try {
     const { data } = await axios.get(`${base}&locate=${postal}&json=1`);
     if (data.latt && data.longt) {
