8ddae1d9898b2a380675f12ec2d68fed50817ff4
Donnie D'Amato <fauxserious@gmail.com>
2020-02-27 22:26:56 -0500

    Get prop data for thresholds

--- api/thresholds.js
+++ api/thresholds.js
@@ -1,9 +1,19 @@
 const { DYNAMO_TABLENAMES } = require('../lib/aws.js');
 const headers = require('../lib/headers.js');
 const Records = require('../lib/records.js');
+const properties = require('../lib/properties.js');
 
 module.exports.handler = async (event, context, callback) => {
-  const thresholds = await new Records({ coords: 'default' }, DYNAMO_TABLENAMES.DZ_THRESHOLDS).get();
+  const records = await new Records({ coords: 'default' }, DYNAMO_TABLENAMES.DZ_THRESHOLDS).get();
+  const thresholds = Object.keys(records).reduce((acc, prop) => {
+    if (prop in properties) {
+      const key = properties[prop].abbr;
+      const value = properties[prop].units(records[prop]);
+      Object.assign(acc, { [key]: value });
+    }
+    return acc;
+  }, {});
+
   response = {
     statusCode: 200,
     headers,
