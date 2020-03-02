1698a039f1bfe95928dfe2c307bc7463e1104b1a
Donnie D'Amato <fauxserious@gmail.com>
2020-03-02 15:58:37 -0500

    Fix result strings

--- api/thresholds.js
+++ api/thresholds.js
@@ -1,16 +1,14 @@
 const { DYNAMO_TABLENAMES } = require('../lib/aws.js');
 const headers = require('../lib/headers.js');
 const Records = require('../lib/records.js');
-const { properties } = require('../lib/properties.js');
+const { properties, createThreshold } = require('../lib/properties.js');
 
 module.exports.handler = async (event, context, callback) => {
   const records = await new Records({ coords: 'default' }, DYNAMO_TABLENAMES.DZ_THRESHOLDS).get();
   const thresholds = Object.keys(records).reduce((acc, prop) => {
     if (prop in properties) {
-      const { convert, units, abbr } = properties[prop];      
-      const converted = convert ? ` (${convert(records[prop])})` : '';
-      const value = `${units(records[prop])}${converted}`;
-      Object.assign(acc, { [abbr]: value });
+      const { abbr } = properties[prop];
+      Object.assign(acc, { [abbr]: createThreshold(records[prop], prop) });
     }
     return acc;
   }, {});
